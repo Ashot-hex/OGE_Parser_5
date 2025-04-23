e57b3d08cf0c89a4c1105f44d146a8c67f7a8481
Ashot-hex <ashotm0571@gmail.com>
2025-04-23 14:42:06 +0200

    boom

--- src/Model/Types/Grades/Elements/GradeCoefficientPair.ts
+++
@@ -1,7 +0,0 @@
-/**Paire de note et de son coefficient */
-export type GradeCoefficientPair = {
-    /**Note */
-    grade: number;
-    /**Coefficient */
-    coefficient: number;
-};
--- src/loader.ts
+++
@@ -1,4 +0,0 @@
-import Injector from "./DependencyInjector";
-import { OverrideGradeDisplay } from "./View/GradeDisplay/Override/OverrideGradeDisplay";
-
-Injector.register("GradeDisplay", OverrideGradeDisplay);
